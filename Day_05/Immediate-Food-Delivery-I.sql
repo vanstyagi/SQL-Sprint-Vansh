a4cf3af09598c36dd2e1e1cb427c0d5ebcf18bab
vanstyagi <vanshtyagi722@gmail.com>
2025-07-06 23:00:40 +0530

    Day 05 ✅ LeetCode 1174 - Immediate Food Delivery I (CONDITIONAL PERCENTAGE + ROUND)

---
+++ Day_05/Immediate-Food-Delivery-I.sql
@@ -0,0 +1,10 @@
+WITH FirstOrders AS (
+    SELECT DISTINCT customer_id,
+           FIRST_VALUE(order_date = customer_pref_delivery_date) OVER (
+               PARTITION BY customer_id
+               ORDER BY order_date
+           ) AS is_immediate
+    FROM Delivery
+)
+SELECT ROUND(AVG(is_immediate) * 100, 2) AS immediate_percentage
+FROM FirstOrders;
